099859682ad73c9074d9cd512c28a5fd7db69f15
Zenil <zenilkapadia20@gmail.com>
2024-05-29 17:53:47 +0530

    added dynamic response

--- backend/models/appointmentSchema.js
+++ backend/models/appointmentSchema.js
@@ -1,82 +1,87 @@
-import mongoose from "mongoose";
-import validator from "validator";
+// models/appointmentSchema.js
+import mongoose from 'mongoose';
+import validator from 'validator';
 
 const appointmentSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'First Name is Required!'],
+    minLength: [3, 'First Name Must contain At Least 3 characters!'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last Name is Required!'],
+    minLength: [3, 'Last Name Must contain At Least 3 characters!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email Is Required'],
+    validate: [validator.isEmail, 'Please Provide A Valid Email!'],
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone Is Required!'],
+    minLength: [10, 'Phone Number Must contain exact 11 Digits!'],
+    maxLength: [10, 'Phone Number Must contain exact 11 Digits!'],
+  },
+  nic: {
+    type: String,
+    required: [true, 'NIC Is Required!'],
+    minLength: [13, 'NIC Must contain exact 13 Digits!'],
+    maxLength: [13, 'NIC Must contain exact 13 Digits!'],
+  },
+  dob: {
+    type: Date,
+    required: [true, 'DOB is required'],
+  },
+  gender: {
+    type: String,
+    required: [true, 'Gender Is Required!'],
+    enum: ['Male', 'Female'],
+  },
+  appointment_date: {
+    type: String,
+    required: true,
+  },
+  select_time: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  doctor: {
     firstName: {
-        type: String,
-        required: [true, "First Name is Required!"],
-        minLength: [3, "First Name Must contain At Least 3 characters!"]
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: [true, "Last Name is Required!"],
-        minLength: [3, "Last Name Must contain At Least 3 characters!"],
+      type: String,
+      required: true,
     },
-    email: {
-        type: String,
-        required: [true, "Email Is Required"],
-        validate: [validator.isEmail, "Please Provide A Valid Email!"],
-    },
-    phone: {
-        type: String,
-        required: [true, "Phone Is Required!"],
-        minLength: [11, "Phone Number Must contain exact 11 Digits!"],
-        maxLength: [11, "Phone Number Must contain exact 11 Digits!"],
-    },
-    nic: {
-        type: String,
-        required: [true, "NIC Is Required!"],
-        minLength: [13, "NIC Must contain exact 13 Digits!"],
-        maxLength: [13, "NIC Must contain exact 13 Digits!"],
-    },
-    dob: {
-        type: Date,
-        required: [true, "DOB is required"],
-    },
-    gender: {
-        type: String,
-        required: [true, "Gender Is Required!"],
-        enum: ["Male", "Female"],
-    },
-    appointment_date: {
-        type: String,
-        required: true,
-    },
-    department: {
-        type: String,
-        required: true,
-    },
-    doctor: {
-        firstName:{
-        type: String,
-        required: true,
-        },
-        lastName:{
-        type: String,
-        required: true,
-        },
-    },
-    hasVisited:{
-        type: Boolean,
-        default: false,
-    },
-    doctorId:{
-        type: mongoose.Schema.ObjectId,
-        required: true,
-    },
-    patientId:{
-        type: mongoose.Schema.ObjectId,
-        required: true,
-    },
-    address:{
-        type: String,
-        required: true,
-    },
-    status:{
-        type: String,
-        enum: ["Pending","Accepted","Rejected"],
-        default: "Pending",
-    },
+  },
+  hasVisited: {
+    type: Boolean,
+    default: false,
+  },
+  doctorId: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+  },
+  patientId: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Accepted', 'Rejected'],
+    default: 'Pending',
+  },
 });
 
-export const Appointment = mongoose.model("Appointment", appointmentSchema);
+export const Appointment = mongoose.model('Appointment', appointmentSchema);
--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
+import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus, getstats } from "../controller/appointmentController.js";
 import {isAdminAuthenticated,isPatientAuthenticated} from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -8,5 +8,5 @@ router.post("/post",isPatientAuthenticated,postAppointment);
 router.get("/getall", isAdminAuthenticated,getAllAppointments);
 router.put("/update/:id", isAdminAuthenticated,updateAppointmentStatus);
 router.delete("/delete/:id", isAdminAuthenticated,deleteAppointment);
-
+router.get("/stats", isAdminAuthenticated, getstats);
 export default router;
